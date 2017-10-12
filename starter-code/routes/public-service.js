7e5952c604a07ee33541d0cb4bc9f041155a9144
polgasull <pol89.gn@gmail.com>
2017-10-12 21:37:16 +0200

    query public service done

--- starter-code/routes/public-service.js
+++ starter-code/routes/public-service.js
@@ -15,7 +15,9 @@ router.get('/public-service/:id', function (req, res, next) {
 
   Service.findOne({ '_id': id }, (err, service) => {
     User.findById({ '_id': service.user }, (err, user) => {
-      Section.find({ 'service': service.service }, (err, section) => {
+      Section.find({ 'service': service._id }, (err, section) => {
+        console.log(section);
+        
         serviceAll = {
           services: service,
           user: user,
@@ -26,38 +28,36 @@ router.get('/public-service/:id', function (req, res, next) {
         } else {
           res.status(200).json(serviceAll);
         }
-      })
-
-    })
-
-  })
-})
+      });
+    });
+  });
+});
 
 // get id user from public service and avoid the change of url on browser.
 
-router.get('/public-service-user/:id', function (req, res, next) {
-  const id = req.params.id;
+// router.get('/public-service-user/:id', function (req, res, next) {
+//   const id = req.params.id;
 
-  User.find({ 'services': id }, (err, user) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.status(200).json(user);
-    }
-  })
-})
+//   User.find({ 'services': id }, (err, user) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.status(200).json(user);
+//     }
+//   })
+// })
 
-router.get('/public-service-section/:id', function (req, res, next) {
-  const id = req.params.id;
+// router.get('/public-service-section/:id', function (req, res, next) {
+//   const id = req.params.id;
 
-  Section.findOne({ 'service': id }, (err, section) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.status(200).json(section);
-    }
-  })
-})
+//   Section.findOne({ 'service': id }, (err, section) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.status(200).json(section);
+//     }
+//   })
+// })
 
 
 
