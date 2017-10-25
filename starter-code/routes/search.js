466466c69a6df16d9637bdd94cafa19065df5ff3
polgasull <pol89.gn@gmail.com>
2017-10-25 23:19:29 +0200

    search ignore case and works

--- starter-code/routes/search.js
+++ starter-code/routes/search.js
@@ -10,18 +10,20 @@ const Contact = require("../models/contact");
 
 router.get('/search-user/:query', function (req, res, next) {
     const query = req.params.query;
-    User.find({ $or: [{ "name": { "$regex": query, "$options": "g" } }] }, (err, usersList) => {
+    User.find({ $or: [{ "name": { "$regex": query, "$options": "g, i" } }] }, (err, usersList) => {
         if (err) {
             res.json(err);
         } else {
             res.status(200).json(usersList);
         }
-    });
+    })
+    .limit(0)
+    .skip(0);
 });
 
 router.get('/search-service/:query', function (req, res, next) {
     const query = req.params.query;
-    Service.find({ $or: [{ "name": { "$regex": query, "$options": "g" } }, { "description": { "$regex": query, "$options": "g" } }] }, (err, serviceList) => {
+    Service.find({ $or: [{ "name": { "$regex": query, "$options": "g, i" } }, { "description": { "$regex": query, "$options": "g" } }] }, (err, serviceList) => {
         if (err) {
             res.json(err);
         } else {
@@ -32,7 +34,7 @@ router.get('/search-service/:query', function (req, res, next) {
 
 router.get('/search-section/:query', function (req, res, next) {
     const query = req.params.query;
-    Section.find({ $or: [{ "name": { "$regex": query, "$options": "g" } }, { "description": { "$regex": query, "$options": "g" } }] }, (err, sectionList) => {
+    Section.find({ $or: [{ "name": { "$regex": query, "$options": "g, i" } }, { "description": { "$regex": query, "$options": "g" } }] }, (err, sectionList) => {
         if (err) {
             res.json(err);
         } else {
